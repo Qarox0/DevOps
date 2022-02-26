b16a40da6aa5a168df6a6b5bc6bc0800ad285502
dozoq <Sebastian.malek@fajne.to>
2022-02-26 20:18:45 +0100

    quick fix

--- DevOps/Assets/Code/UI/Menu/MenuManager.cs
+++ DevOps/Assets/Code/UI/Menu/MenuManager.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private Button _newGameButton;
+    [SerializeField] private Button _twitterButton;
+    [SerializeField] private Button _discordButton;
+    [SerializeField] private Button _exitButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _newGameButton.onClick.AddListener(StartNewGame);
+        _discordButton.onClick.AddListener(OpenDiscord);
+        _twitterButton.onClick.AddListener(OpenTwiter);
+        _exitButton.onClick.AddListener(ExitGame);
     }
 
     // Update is called once per frame
@@ -17,9 +25,26 @@ public class MenuManager : MonoBehaviour
         
     }
 
-    public void StartNewGame()
+    private void StartNewGame()
     {
         PlayerPrefs.SetInt("NewGame", 1);
         SceneManager.LoadScene("Scenes/Mapa");
     }
+
+    private void OpenTwiter()
+    {
+        Debug.LogError("Not linked yet");
+        Application.OpenURL("http://unity3d.com/");
+    }
+    
+    private void OpenDiscord()
+    {
+        Application.OpenURL("https://discord.gg/xKBvaAKU");
+    }
+    
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
